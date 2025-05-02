e36fe943873b13623b9d919a9770e37054473257
Faizan <faizanfirdousi.official@gmail.com>
2025-05-02 12:55:04 +0530

    unmarshal the json,wrote the struct,formatted the output

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
+
+type Weather struct{
+	Location struct{
+		Name string `json:"name`
+		Country string `json:"country"`
+	} `json:"location"`
+
+	Current struct{
+		TempC float64 `json:"temp_c"`
+		Condition struct{
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+
+	Forecast struct{
+		Forecastday [] struct{
+			Hour []struct{
+				TimeEpoch int64 `json:"time_epoch"`
+				TempC float64 `json:"temp_c"`
+				Condition struct{
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday`
+	} `json:"forecast"`
+}
+
 
 func main() {
 	// Load environment variables from .env
@@ -40,5 +70,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body,&weather)
+	if err!=nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf(
+		"%s, %s: %.0fC, %s\n",
+		location.Name,
+		location.Country,
+		current.TempC,
+		current.Condition.Text,
+	)
+
+	for _,hour := range hours{
+		date := time.Unix(hour.TimeEpoch,0)
+
+		if date.Before(time.Now()){
+			continue
+		}
+		fmt.Printf(
+			"%s - %.0fC,  %0.f%%,  %s\n",
+			date.Format("15:04"),
+			hour.TempC,
+			hour.ChanceOfRain,
+			hour.Condition.Text,
+		)
+	}
+
 }

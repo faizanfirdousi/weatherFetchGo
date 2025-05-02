042fc6709356a493a32486df4e4fbb44b81c81c2
Faizan <faizanfirdousi.official@gmail.com>
2025-05-02 11:37:35 +0530

    fixed the hardcoded API key

--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
-	"net/http"
-	"io"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-
-
-func main(){
-	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=994231216f7a4852841212932250105&q=18.584320985033493,73.73586345409191&days=1&aqi=no&alerts=no")
+func main() {
+	// Load environment variables from .env
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal("Error loading .env file")
 	}
 
+	apiKey := os.Getenv("WEATHER_API_KEY")
+	if apiKey == "" {
+		log.Fatal("WEATHER_API_KEY not set in environment")
+	}
+
+	// Construct the full URL
+	url := fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=18.584320985033493,73.73586345409191&days=1&aqi=no&alerts=no", apiKey)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		panic("Weather API not available")
+		log.Fatalf("Weather API not available: Status %d", res.StatusCode)
 	}
 
 	body, err := io.ReadAll(res.Body)
-	if err != nil{
-		panic(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println(string(body))
